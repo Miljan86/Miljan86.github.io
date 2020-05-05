358327f7046d3a3386370e41e3bf5e981d5224dc
Miljan86 <miljan86stojanovic@yahoo.com>
2020-05-05 22:35:09 +0200

    Changed players cards

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,40 +8,27 @@
 //     $(event.currentTarget.previousElementSibling).toggleClass('d-none');
 // });
 
-const toggleCard = event => {
-    $(event.currentTarget).toggleClass('d-none');
-    $(event.currentTarget.nextElementSibling).toggleClass('d-none');
-};
+// const toggleCard = event => {
+//     $(event.currentTarget).toggleClass('d-none');
+//     $(event.currentTarget.nextElementSibling).toggleClass('d-none');
+// };
 
-$('.card1').mouseenter(toggleCard);
-$('.card1').mouseleave(toggleCard);
+// $('.card1').mouseenter(toggleCard);
+// $('.card1').mouseleave(toggleCard);
 
-// TODO tap event for smartphone
-$('.card1').on('tap', toggleCard);
+// // TODO tap event for smartphone
+// $('.card1').on('tap', toggleCard);
 
-// $(document).on("pagecreate", function(){
-//     $(".card1").on("tap",function(){
-//         console.log(this);
-//       $(this).toggleClass('d-none');
-//       $(this.nextElementSibling).toggleClass('d-none');
-//     });  
-//     // $(".card2").on("tap",function(){
-//     //     console.log(this);
-//     //     $(this).toggleClass('d-none');
-//     //     $(this.previousElementSibling).toggleClass('d-none');
-//     // });                         
-//   }); 
-
-$('.card1').on('tap',function(){
-    console.log(this);
-  $(this).toggleClass('d-none');
-  $(this.nextElementSibling).toggleClass('d-none');
-}); 
+// $('.card1').on('tap',function(){
+//     console.log(this);
+//   $(this).toggleClass('d-none');
+//   $(this.nextElementSibling).toggleClass('d-none');
+// }); 
 
 
 
 
-// When the user clicks on the button, scroll to the top of the document
+// Scroll - start
 function goToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
@@ -50,15 +37,14 @@ function goToTop() {
 $(window).scroll(function () {
 
     const mybutton = $('#topBtn');
-
+    // Show back to the top icon
     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        // mybutton.style.display = "block";
         mybutton.addClass('d-sm-block');
     } else {
-        // mybutton.style.display = "none";
         mybutton.removeClass('d-sm-block');
     }
 
+    // Show splash image
     $('.splash>').each(function () {
         var imagePos = $(this).offset().top;
         var imageHeight = $(this).height();
@@ -72,8 +58,57 @@ $(window).scroll(function () {
 
     });
 });
+// Cards - end
 
 
+// Cards - strat
+var $cards = $('.card-object'),
+    $faceButtons = $('.face');
 
+$faceButtons.on('click', flipCard);
 
+function flipCard(event) {
+    event.preventDefault();
+
+    var $btnFace = $(this),
+        $card = $btnFace.parent('.card-object');
+
+    if ($card.hasClass('flip-in')) {
+        closeCards();
+    } else {
+        closeCards();
+        openCard($card);
+    }
+
+}
+
+function closeCards() {
+    $cards.each(function () {
+        $(this)
+            .filter('.flip-in')
+            .removeClass('flip-in')
+            .queue(function () {
+                // Force reflow hack
+                var reflow = this.offsetHeight;
+                $(this)
+                    .addClass('flip-out')
+                    .dequeue();
+            })
+
+    });
+}
+
+function openCard($card) {
+    $card
+        .removeClass('flip-out')
+        .queue(function () {
+            // Force reflow hack
+            var reflow = this.offsetHeight;
+            $(this)
+                .addClass('flip-in')
+                .dequeue();
+        });
+
+}
+// Cards - end
 
