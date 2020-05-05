c032ae650148e4b519004e41d204d4e8db52e398
Miljan86 <miljan86stojanovic@yahoo.com>
2020-05-05 12:34:25 +0200

    Added video, back to the top button

--- resources/js/script.js
+++ resources/js/script.js
@@ -37,3 +37,43 @@ $('.card1').on('tap',function(){
   $(this).toggleClass('d-none');
   $(this.nextElementSibling).toggleClass('d-none');
 }); 
+
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+$(window).scroll(function () {
+
+    const mybutton = $('#topBtn');
+
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        // mybutton.style.display = "block";
+        mybutton.addClass('d-sm-block');
+    } else {
+        // mybutton.style.display = "none";
+        mybutton.removeClass('d-sm-block');
+    }
+
+    $('.splash>').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).removeClass("visible");
+        } else {
+            $(this).addClass("visible");
+        }
+
+    });
+});
+
+
+
+
+
